2f1bfde29474c2edb6a347a7ef95c862ef620d71
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-10 18:39:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,15 +2,15 @@ class Person
   def name=(name)
     @name = name
   end
-  
+
   def name
-    @name 
+    @name
   end
   def job=(job)
     @job = job
   end
-  
+
   def job
     @job
-  end 
+  end
 end
