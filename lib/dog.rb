6be86483191e11415bb8a5e6f237f7dea6dd1ced
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-10 18:37:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,15 @@ class Dog
   def name=(dogs_name)
       @name = dogs_name
     end
-    
+
     def name
-      @name 
+      @name
     end
     def breed=(breed)
       @breed = breed
     end
-    
+
     def breed
       @breed
-    end 
+    end
 end
